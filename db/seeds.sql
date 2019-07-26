19ee1d8a23dc8174c765b787dd51c721f17db78e
MesayBeyene <mesibella@gmail.com>
2019-07-25 21:19:42 -0700

    second commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Happy Hour');
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Chicken');
+INSERT INTO burgers(burger_name) VALUES ('Cheese');
+
+
